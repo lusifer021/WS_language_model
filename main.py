cad47059e551f40b7ed43194e3d7e6a248c0fef4
Mrinmoy Mahato <mrinmoymahato2001@gmail.com>
2022-12-07 00:09:37 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from nltk.tokenize import word_tokenize
+
+words = ['fuck','hookup', 'hook-up', 'sex' , 'hook']
+
+app = FastAPI()
+
+from pydantic import BaseModel
+
+class Msg(BaseModel):
+    message: str
+
+@app.get("/")
+def test():
+        return {"working"}
+
+@app.post("/detect/")
+async def detect(item: Msg):
+    message = item.message
+
+    flag=1
+    blur = []
+    tk = word_tokenize(message)
+    for w in words:
+
+        if(w in tk):
+            flag=0
+            blur.append(w)
+            # break
+    
+    
+    if flag==0:
+        ff = 'Yes'
+        dict1 = {ff:blur}
+    else:
+        ff = 'No'
+        dict1 = {ff : blur}
+
+    return dict1
